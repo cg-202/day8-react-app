0aeb132a64a948057194d2fec6c17de29461e7a7
santosh <santosh.ece06@gmail.com>
2021-07-01 09:46:14 +0530

    b1. basics of Component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
+/**
+ * SPECIAL FUNCTION. ITS RETURNS JSX.
+ *
+ * METHOD
+ * COMPONENT BOY/GIRL
+ * Method Name :: Starts with Capital
+ * React Component
+ *
+ * This function is creating a CUSTOM TAG / COMPONENT.
+ * React Tags Starts with CAPITAL LETTER
+ * <App />
+ *
+ * HTML Has A SET OF PREFDEINFED TAGS :: Can we create CUSTOM TAGS?
+ * <TAG-NAME></TAG-NAME>
+ * <div></div>
+ * <h1></h1>
+ */
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
 
 export default App;
